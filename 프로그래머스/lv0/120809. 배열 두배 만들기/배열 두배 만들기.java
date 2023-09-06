8393af3df9e0f568d4614b0f583a93db88769ce6
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-06 15:21:31 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.54 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        return Arrays.stream(numbers).map(v -> v * 2).toArray();
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
 }
