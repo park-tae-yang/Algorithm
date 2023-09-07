67d87cc21ddcf1570a0708cba5d756ba2569a947
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-07 18:02:32 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for(int i = 0; i < str.length(); i++) answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        return answer;
+    }
+}
