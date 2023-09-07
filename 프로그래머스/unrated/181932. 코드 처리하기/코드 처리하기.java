d205f97d4d16c24f283b9eb4a601f4562f88bf7c
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-07 16:22:35 +0900

    [unrated] Title: 코드 처리하기, Time: 0.04 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i = 0; i < code.length(); i++) {
+            if(mode == 0) {
+                if(code.charAt(i) == '1') {
+                    mode = 1;
+                    continue;
+                }
+                if(i % 2 == 0) answer += code.charAt(i);
+            }
+            else {
+                if(code.charAt(i) == '1') {
+                    mode = 0;
+                    continue;
+                }
+                if(i % 2 == 1) answer += code.charAt(i);
+            }
+        }
+        if(answer.isEmpty()) return "EMPTY";
+        return answer;
+    }
+}
