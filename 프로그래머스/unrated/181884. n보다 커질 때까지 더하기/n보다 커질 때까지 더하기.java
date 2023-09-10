e8fb5fcc839294d66c447dab6e1b6b1aedf63c6f
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-10 14:07:02 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for(int i : numbers) {
+            if(answer <= n) answer += i;
+        }
+        return answer;
+    }
+}
