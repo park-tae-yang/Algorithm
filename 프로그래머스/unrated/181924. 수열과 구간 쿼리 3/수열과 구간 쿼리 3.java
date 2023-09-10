b580ab7f7029584594625c3aa5c71a123754b091
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-10 16:14:38 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -3,8 +3,8 @@ class Solution {
         for(int i = 0; i < queries.length; i++) {
             int c = queries[i][0];
             int d = queries[i][1];
-            int a = arr[queries[i][0]];
-            int b = arr[queries[i][1]];
+            int a = arr[c];
+            int b = arr[d];
             arr[d] = a;
             arr[c] = b;
         }
