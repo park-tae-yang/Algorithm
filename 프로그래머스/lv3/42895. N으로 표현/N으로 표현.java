2858e71d3d18f0cbdceee97a2f1fa5ddb946412d
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-09-21 21:55:09 +0900

    [level 3] Title: N으로 표현, Time: 0.14 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -1,30 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        List<Set<Integer>> countList = new ArrayList<>();
-        for(int i=0; i<9; i++) countList.add(new HashSet<>());
-        countList.get(1).add(N);
+        List<Set<Integer>> idxList = new ArrayList<>();
+        for(int i=0; i<9; i++) idxList.add(new HashSet<>());
+        idxList.get(1).add(N);
         for (int i = 2; i < 9; i++) {
-            Set<Integer> countSet = countList.get(i);
+            Set<Integer> idxSet = idxList.get(i);
             for (int j = 1; j < i; j++) {
-                Set<Integer> preSet = countList.get(j);
-                Set<Integer> postSet = countList.get(i-j);
-                for (int preNum: preSet) {
-                    for (int postNum : postSet) {
-                        countSet.add(preNum + postNum);
-                        countSet.add(preNum - postNum);
-                        countSet.add(preNum * postNum);
+                Set<Integer> jSet = idxList.get(j);
+                Set<Integer> iSet = idxList.get(i-j);
+                for (int jNum: jSet) {
+                    for (int iNum : iSet) {
+                        idxSet.add(jNum + iNum);
+                        idxSet.add(jNum - iNum);
+                        idxSet.add(jNum * iNum);
 
-                        if (preNum != 0 && postNum != 0) {
-                            countSet.add(preNum / postNum);
+                        if (jNum != 0 && iNum != 0) {
+                            idxSet.add(jNum / iNum);
                         }
                     }
                 }
-                countSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+                idxSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
             }
-            for (Set<Integer> sub : countList) {
-                if (sub.contains(number)) {
-                    return countList.indexOf(sub);
+            for (Set<Integer> idx : idxList) {
+                if (idx.contains(number)) {
+                    return idxList.indexOf(idx);
                 }
             }
         }
