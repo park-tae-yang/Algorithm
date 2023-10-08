33d64257404410ee3dffa89ad989f8a4c37036c6
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-10-08 18:13:03 +0900

    [level 2] Title: 숫자의 표현, Time: 1.06 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++) {
+            int sum = i;
+            for(int j = i + 1; j <= n; j++) {
+                sum += j;
+                if(sum == n) {
+                    answer++;
+                    break;
+                }
+                if(sum > n) {
+                    break;
+                }
+            }
+        }
+        return answer + 1;
+    }
+}
