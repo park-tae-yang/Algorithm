5b70ccad6b4f76e44e1eb0b874d2240b331b5e74
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-10-10 16:54:27 +0900

    [level 2] Title: 짝지어 제거하기, Time: 106.42 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(!stack.empty() && stack.peek() == c){
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+        
+        return (stack.empty() ? 1 : 0);
+    }
+}
