45a335153d9226bb48764fdbbbfefc7c8b69912a
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-10-10 23:15:15 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(true) {
+            if(n == 1) {
+                ans++;
+                break;
+            }
+            if(n % 2 == 0) {
+                n /= 2;
+                continue;
+            }
+            n--;
+            ans++;
+        }
+
+        return ans;
+    }
+}
