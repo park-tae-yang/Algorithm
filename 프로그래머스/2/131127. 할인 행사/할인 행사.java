eae0f003d9e2458141a1476a578c87b8011ebe08
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-10-12 16:34:11 +0900

    [level 2] Title: 할인 행사, Time: 1.67 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,14 +4,8 @@ class Solution {
         int answer = 0;
         for(int i = 0; i < discount.length; i++) {
             for(int j = 0; j < want.length; j++) {
-                if(discount[i].equals(want[j])) {
-                    number[j]--;
-                }
-                if(i >= 10) {
-                    if(discount[i - 10].equals(want[j])) {
-                        number[j]++;
-                    }
-                }
+                if(discount[i].equals(want[j])) number[j]--;
+                if(i >= 10) if(discount[i - 10].equals(want[j])) number[j]++;
             }
             answer += Arrays.stream(number).allMatch(item -> item == 0) ? 1 : 0;
         }
