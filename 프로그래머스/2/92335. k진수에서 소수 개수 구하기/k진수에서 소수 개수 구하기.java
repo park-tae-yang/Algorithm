65dc5da838095ac236eea42ce515cd7b866607ab
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-11-09 19:55:39 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.71 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String nnum = Integer.toString(n,k) + "0";
+        String now = "";
+        loop: for(int i = 0; i < nnum.length(); i++) {
+            if(nnum.charAt(i) != '0') {
+                now += nnum.charAt(i);
+            } else if(!now.isEmpty()) {
+                long toNum = Long.parseLong(now);
+                if (toNum == 1) {
+                    now = "";
+                    continue;
+                }
+                for(int j = 2; j < (int)Math.sqrt(toNum) + 1; j++) {
+                    if(toNum % j == 0) {
+                        now = "";
+                        continue loop;
+                    }
+                }
+                answer++;
+                now = "";
+            }
+        }
+        return answer;
+    }
+}
