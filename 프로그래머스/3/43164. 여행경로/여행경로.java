4fc2a7cf39857134f4de0300b33d757d2f0b7fcf
park-tae-yang <124670911+park-tae-yang@users.noreply.github.com>
2023-11-15 23:12:54 +0900

    [level 3] Title: 여행경로, Time: 0.47 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    static String[] answer;
+
+    public String[] solution(String[][] tickets) {
+        answer = new String[tickets.length + 1];
+        visited = new boolean[tickets.length];
+        answer[0] = "ICN";
+        answer[tickets.length] = "";
+        Arrays.sort(tickets, new Comparator<String[]>() {
+
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o1[0].toString().contentEquals(o2[0].toString()))
+                    return o1[1].toString().compareTo(o2[1].toString());
+                else
+                    return o1[0].toString().compareTo(o2[0].toString());
+            }
+        });
+
+        dfs(1, tickets, "ICN");
+        return answer;
+    }
+
+    public void dfs(int depth,String[][] tickets,String ap) {
+        for(int i = 0; i < tickets.length; i++) {
+            if (!answer[tickets.length].isEmpty()) return;
+            if(visited[i] || !tickets[i][0].equals(ap)) continue;
+            visited[i] = true;
+            answer[depth] = tickets[i][1];
+            dfs(depth + 1,tickets,tickets[i][1]);
+            visited[i] = false;
+        }
+    }
+}
